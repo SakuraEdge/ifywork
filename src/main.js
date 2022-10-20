b9428ff4741e4aeb63149ac381fb0eec22c5d132
lumada <1165743986@qq.com>
2022-10-20 11:19:41 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import router from 'vue-router'
 
 createApp(App).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app')
