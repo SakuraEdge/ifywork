1aca237da4eee903333645c46b16b48fdf9911b2
Edgeworthia <351945458@qq.com>
2022-10-20 16:20:16 +0800

    fixs 2022/10/20 16.20

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy:{
+      '/api':{
+        target:'http://127.0.0.1:8081/servletdemo', //后台接口地址
+        ws:true, //如果要代理 websockets，配置这个参数
+        secure:false,     //如果是https接口，需要配置这个参数
+        changeOrigin:true, //是否跨域
+        pathRewrite:{         //重写路径
+         '^/api':''
+      }
+}
+}
+}
 })
