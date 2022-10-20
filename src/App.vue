41c16d2d89e29c955f8afb4db38a7a4e41ee21cc
lumada <1165743986@qq.com>
2022-10-20 11:19:41 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="assets/picture/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -13,14 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="outer_label">
+      <img class="inner_label login_logo" src="../assets/logo.png">
+    </div>
+    <div class="login_form">
+      <input type="text"  class="qxs-ic_user qxs-icon"  placeholder="用户名" v-model="userName">
+      <input type="text"  class="qxs-ic_password qxs-icon"  placeholder="密码" v-model="password">
+      <el-button class="login_btn" type="primary" round :loading="isBtnLoading">登录</el-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      userName: '',
+      password: '',
+      isBtnLoading: false
+    }
+  },
+  created() {
+    if (JSON.parse(localStorage.getItem('user')) && JSON.parse(localStorage.getItem('user')).userName) {
+      this.userName = JSON.parse(localStorage.getItem('user')).userName;
+      this.password = JSON.parse(localStorage.getItem('user')).password;
+    }
+  },
+  computed: {
+    btnText() {
+      if (this.isBtnLoading) return '登录中...';
+      return '登录';
+    }
+  },
+  methods: {
+    login() {
+      if (!this.userName) {
+        this.$message.error('请输入用户名');
+        return;
+      }
+      if (!this.password) {
+        this.$message.error('请输入密码');
+        return;
+      }
+      //  开始真正的登陆请求
+
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.login_form {
+  padding-top: 5%;
+  padding-left: 35%;
+  padding-right: 10%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.qxs-ic_user {
+  background-size: 13px 15px;
+  background: no-repeat 3%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.qxs-ic_password {
+  background-size: 13px 15px;
+  background: no-repeat 3%;
+  margin-bottom: 20px;
 }
-a {
-  color: #42b983;
+.login_logo {
+  height: 100%;
+}
+.login_btn {
+  margin-left: 5%;
+  width: 50%;
+  font-size: 16px;
+  background: -webkit-linear-gradient(left, #000099, #2154FA); /* Safari 5.1 - 6.0 */
+  background: -o-linear-gradient(right, #000099, #2154FA); /* Opera 11.1 - 12.0 */
+  background: -moz-linear-gradient(right, #000099, #2154FA); /* Firefox 3.6 - 15 */
+  background: linear-gradient(to right, #000099 , #2154FA); /* 标准的语法 */
+  filter: brightness(1.4);
 }
 </style>
