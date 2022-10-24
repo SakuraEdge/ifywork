667c7156d109b0aa0dc4d3accd0b224e1b6a40ec
SakuraEdge <351945458@qq.com>
2022-10-24 16:23:11 +0800

    fixs 2022/10/24 16.20

---
+++ src/components/NewFile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <h1>
+        这是新页面喵
+      </h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "NewFile"
+}
+</script>
+
+<style scoped>
+
+</style>
